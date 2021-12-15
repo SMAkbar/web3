3b30680ceaa158e4752512315a6dcd7b6a1e7710
Syed Muhammad Akbar <sm.akbar42@gmail.com>
2021-12-15 18:19:13 +0500

    test implemented and working

--- demos/brownie_simple_storage/scripts/deploy.py
+++ demos/brownie_simple_storage/scripts/deploy.py
@@ -1,10 +1,21 @@
-from brownie import accounts, config
-import os
+from brownie import accounts, config, SimpleStorage
 
 
 def deploy_simple_storage():
-    account = accounts.add(config["wallets"]["from_key"])
-    print(account)
+    # account = accounts.add(config["wallets"]["from_key"])
+    # print(account)
+
+    account = accounts[0]
+    simple_storage = SimpleStorage.deploy({"from": account})
+    # trax or call
+    # print(simple_storage)
+    store_value = simple_storage.retreive()
+    print(store_value)
+
+    transaction = simple_storage.store(15, {"from": account})
+    transaction.wait(1)
+    updated_store_value = simple_storage.retreive()
+    print(updated_store_value)
 
 
 def main():
