fe668700af83e902ca0f1b3e90df597fc78bb695
Syed Muhammad Akbar <sm.akbar42@gmail.com>
2021-12-15 17:25:38 +0500

    doesn't work

--- demos/web3_py_simple_storage/deploy.py
+++ demos/web3_py_simple_storage/deploy.py
@@ -40,27 +40,18 @@ abi = compile_sol["contracts"]["SimpleStorage.sol"]["SimpleStorage"]["abi"]
 
 # connecting to rinkeby
 w3 = Web3(
-    Web3.HTTPProvider("https://rinkeby.infura.io/v3/b27eb5f9ea0840a7ac7bcf10697c17bc")
+    Web3.HTTPProvider("https://rinkeby.infura.io/v3/b27eb5f9ea0840a7ac7bcf10697c17bcs")
 )
 chain_id = 4  # can be found on internet... chain id for rinkeby
 my_address = "0x9BA7D5E019c2cA8C1ADd76980dd8da7379b68b98"
 private_key = os.getenv("PRIVATE_KEY")
-print(private_key)
+# print(private_key)
 
-
-# create contract in python
+# Create the contract in Python
 SimpleStorage = w3.eth.contract(abi=abi, bytecode=bytecode)
-print(SimpleStorage)
-
-
-# get the latest transaction
+# Get the latest transaction
 nonce = w3.eth.getTransactionCount(my_address)
-print(nonce)
-
-# 1. Build a transaction
-# 2. Sign a transaction
-# 3. Send a transaction
-
+# Submit the transaction that deploys the contract
 transaction = SimpleStorage.constructor().buildTransaction(
     {
         "chainId": chain_id,
@@ -69,30 +60,21 @@ transaction = SimpleStorage.constructor().buildTransaction(
         "nonce": nonce,
     }
 )
-print(transaction)
-
+# Sign the transaction
 signed_txn = w3.eth.account.sign_transaction(transaction, private_key=private_key)
-
-# send transaction
+print("Deploying Contract!")
+# Send it!
 tx_hash = w3.eth.send_raw_transaction(signed_txn.rawTransaction)
-
-# block confirmation --- wait for transaction to complete
+# Wait for the transaction to be mined, and get the transaction receipt
+print("Waiting for transaction to finish...")
 tx_receipt = w3.eth.wait_for_transaction_receipt(tx_hash)
+print(f"Done! Contract deployed to {tx_receipt.contractAddress}")
 
 
-# working with contract you always need
-# contract address
-# contract abi
-
+# Working with deployed Contracts
 simple_storage = w3.eth.contract(address=tx_receipt.contractAddress, abi=abi)
-# call -> Simulate making a call and getting a value
-# transact -> Make a state change
-
-# initial value of contract
-print(simple_storage.functions.retreive().call())
-
-# transction
-store_transaction = simple_storage.functions.store(15).buildTransaction(
+print(f"Initial Stored Value {simple_storage.functions.retrieve().call()}")
+greeting_transaction = simple_storage.functions.store(15).buildTransaction(
     {
         "chainId": chain_id,
         "gasPrice": w3.eth.gas_price,
@@ -100,12 +82,11 @@ store_transaction = simple_storage.functions.store(15).buildTransaction(
         "nonce": nonce + 1,
     }
 )
-signed_store_txn = w3.eth.account.sign_transaction(
-    store_transaction, private_key=private_key
+signed_greeting_txn = w3.eth.account.sign_transaction(
+    greeting_transaction, private_key=private_key
 )
-transaction_hash = w3.eth.send_raw_transaction(signed_store_txn.rawTransaction)
-transaction_receipt = w3.eth.wait_for_transaction_receipt(transaction_hash)
+tx_greeting_hash = w3.eth.send_raw_transaction(signed_greeting_txn.rawTransaction)
+print("Updating stored Value...")
+tx_receipt = w3.eth.wait_for_transaction_receipt(tx_greeting_hash)
 
-
-# initial value of contract
-print(simple_storage.functions.retreive().call())
+print(simple_storage.functions.retrieve().call())
