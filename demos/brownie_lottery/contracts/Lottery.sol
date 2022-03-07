dd923e97883502c88ffcc5214478fb6272ee913a
Syed Muhammad Akbar <sm.akbar42@gmail.com>
2022-03-07 21:43:29 +0500

    ..

--- demos/brownie_lottery/contracts/Lottery.sol
+++ demos/brownie_lottery/contracts/Lottery.sol
@@ -2,27 +2,91 @@
 pragma solidity ^0.6.6;
 
 import "@chainlink/contracts/src/v0.6/interfaces/AggregatorV3Interface.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@chainlink/contracts/src/v0.6/VRFConsumerBase.sol";
 
-contract Lottery {
+contract Lottery is Ownable, VRFConsumerBase {
     address payable[] public players;
+    address payable public recentWinner;
     uint256 public usdEntryFee;
+    uint256 public randomness;
 
     AggregatorV3Interface internal ethUSDPriceFeed;
 
-    constructor(address _priceFeedAddress) public {
+    enum LOTTERY_STATE {
+        OPEN,
+        CLOSE,
+        CACLUCATING_WINNER
+    }
+    LOTTERY_STATE public lottery_state;
+
+    uint256 public fee;
+    bytes32 public keyhash;
+
+    constructor(
+        address _priceFeedAddress,
+        address _vrfCoordinator,
+        address _link,
+        uint256 _fee,
+        bytes32 _keyhash
+    ) public VRFConsumerBase(_vrfCoordinator, _link) {
         usdEntryFee = 50 * (10**18);
         ethUSDPriceFeed = AggregatorV3Interface(_priceFeedAddress);
+        lottery_state = LOTTERY_STATE.CLOSE;
+        fee = _fee;
+        keyhash = _keyhash;
     }
 
     function enter() public payable {
         // $50 min
         // require(condition);
+        require(lottery_state == LOTTERY_STATE.OPEN);
+        require(msg.value >= getEntranceFee(), "Not enough Fee");
         players.push(msg.sender);
     }
 
-    function startLottery() public {}
+    function startLottery() public {
+        require(
+            lottery_state == LOTTERY_STATE.CLOSE,
+            "can't start a new lottery yet!"
+        );
+        lottery_state = LOTTERY_STATE.OPEN;
+    }
 
-    function endLottery() public {}
+    function endLottery() public onlyOwner {
+        // uint256(
+        //     keccack256(
+        //         abi.encodePacked(
+        //             nonce, // predictable
+        //             msg.sender, // predictable
+        //             block.difficulty, // can actually be manipulated by the miners!
+        //             block.timestamp // predictable
+        //         )
+        //     )
+        // ) % players.length;
+        lottery_state = LOTTERY_STATE.CACLUCATING_WINNER;
+        bytes32 requestId = requestRandomness(keyhash, fee);
+    }
+
+    function fulfillRandomness(bytes32 _requestId, uint256 _randomness)
+        internal
+        override
+    {
+        require(
+            lottery_state == LOTTERY_STATE.CACLUCATING_WINNER,
+            "You aren't there yet"
+        );
+        require(_randomness > 0, "random-not-found");
+
+        uint256 indexOfWinner = _randomness % players.length;
+        recentWinner = players[indexOfWinner];
+        recentWinner.transfer(address(this).balance);
+        randomness = _randomness;
+
+        // Reset
+        players = new address payable[](0);
+        lottery_state = LOTTERY_STATE.CLOSE;
+    }
 
     /**
      * Returns the latest price
